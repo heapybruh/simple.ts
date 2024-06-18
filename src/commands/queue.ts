1e735b23e8d11cc7635713dd68cd2723d812e2b8
heapy <63428380+heapybruh@users.noreply.github.com>
2024-06-18 14:21:01 +0200

    Update queue.ts

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -132,7 +132,7 @@ export class Queue {
                 iconURL: process.env.QUEUE_PATH,
               })
               .setColor(color)
-              .setDescription("Use **/queue** again to get the queue.")
+              .setDescription("Use **/queue** again to get the queue")
               .setFooter({
                 text: `@${interaction.member?.user.username} used /queue`,
                 iconURL: process.env.LOGO_PATH,
